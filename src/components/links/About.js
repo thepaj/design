d72e9cbc458da330e1779501b4a19c6e7a959d45
thepaj <pavla.jureckova@gmail.com>
2023-03-01 15:51:55 +0100

    new new commit

--- src/components/links/About.js
+++ src/components/links/About.js
@@ -3,12 +3,16 @@ import Dena from '../../images/dn.jpg';
 function About() {
     return (
       <div className="About">
-        <div className="about-card">
-          <div className="imgBx">
-              <img src={Dena} alt="images" />
+        <div className='about-container'>
+          <div className='about-card'>
+            <img src={Dena} alt='dena' className='dena-image'/>
           </div>
-          <div className="details">
-              <h2>SomeOne Famous<br/><span>Director</span></h2>
+          <div className='about-card'>
+            <div className='about-text'>
+              <p>
+              Aenean feugiat in ligula ut scelerisque. Proin erat erat, ultricies ut bibendum quis, egestas quis enim. Ut ornare erat in odio tempus, id eleifend tortor rutrum. Vivamus pulvinar neque vestibulum fringilla varius. Vestibulum purus ante, venenatis sed nulla et, feugiat luctus massa. Quisque consequat imperdiet magna, bibendum fermentum neque malesuada eget. Sed vitae quam vitae nisi imperdiet feugiat vel quis lorem. In sit amet elementum mi. Morbi scelerisque mauris risus, sed suscipit massa dictum non. Integer egestas nec libero quis consectetur. Fusce non erat purus. 
+              </p>
+            </div>
           </div>
         </div>
       </div>
